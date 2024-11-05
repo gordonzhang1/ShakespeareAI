888b0dd38d20b98372ebbc38ebc90ee037647e35
Gordon06060 <gordonz0913@gmail.com>
2024-11-05 17:29:06 -0500

    Implemented Login for front page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,128 +4,105 @@ import "../Login.css";
 export default function Login() {
   return (
     <>
-      <section className="vh-100">
-        <div className="container-fluid h-custom">
-          <div className="outercontainer-2">
-            <div className="col-md-9 col-lg-6 col-xl-5">
+      <div className="container d-flex justify-content-center align-items-center min-vh-100">
+        <div className="row border rounded-5 p-3 bg-white shadow box-area">
+          <div
+            className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+            style={{ background: "#103cbe" }}
+          >
+            <div className="featured-image mb-3">
               <img
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                alt="Featured"
                 className="img-fluid"
-                alt="Sample image"
+                style={{ width: "250px" }}
               />
             </div>
-            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form>
-                <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                  <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-floating mx-1"
-                  >
-                    <i className="fab fa-facebook-f"></i>
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-floating mx-1"
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-floating mx-1"
-                  >
-                    <i className="fab fa-linkedin-in"></i>
-                  </button>
-                </div>
+            <p
+              className="text-white fs-2"
+              style={{
+                fontFamily: "'Courier New', Courier, monospace",
+                fontWeight: 600,
+              }}
+            >
+              Sign up
+            </p>
+            <small
+              className="text-white text-wrap text-center"
+              style={{
+                width: "17rem",
+                fontFamily: "'Courier New', Courier, monospace",
+              }}
+            >
+              The new AI Note-Taking Platform.
+            </small>
+          </div>
 
-                <div className="divider d-flex align-items-center my-4">
-                  <p className="text-center fw-bold mx-3 mb-0">Or</p>
-                </div>
-
-                <div className="form-outline mb-4">
+          <div className="col-md-6 right-box">
+            <div className="row align-items-center">
+              <div className="header-text mb-4">
+                <h2>Hello, Again</h2>
+                <p>We are happy to have you back.</p>
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control form-control-lg bg-light fs-6"
+                  placeholder="Email address"
+                />
+              </div>
+              <div className="input-group mb-1">
+                <input
+                  type="password"
+                  className="form-control form-control-lg bg-light fs-6"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="input-group mb-5 d-flex justify-content-between">
+                <div className="form-check">
                   <input
-                    type="email"
-                    id="form3Example3"
-                    className="form-control form-control-lg"
-                    placeholder="Enter a valid email address"
+                    type="checkbox"
+                    className="form-check-input"
+                    id="formCheck"
                   />
-                  <label className="form-label" htmlFor="form3Example3">
-                    Email address
+                  <label
+                    htmlFor="formCheck"
+                    className="form-check-label text-secondary"
+                  >
+                    <small>Remember Me</small>
                   </label>
                 </div>
-
-                <div className="form-outline mb-3">
-                  <input
-                    type="password"
-                    id="form3Example4"
-                    className="form-control form-control-lg"
-                    placeholder="Enter password"
+                <div className="forgot">
+                  <small>
+                    <a href="#">Forgot Password?</a>
+                  </small>
+                </div>
+              </div>
+              <div className="input-group mb-3">
+                <button className="btn btn-lg btn-primary w-100 fs-6">
+                  Login
+                </button>
+              </div>
+              <div className="input-group mb-3">
+                <button className="btn btn-lg btn-light w-100 fs-6">
+                  <img
+                    src="../assets/google.png"
+                    alt="Google Sign In"
+                    style={{ width: "20px" }}
+                    className="me-2"
                   />
-                  <label className="form-label" htmlFor="form3Example4">
-                    Password
-                  </label>
-                </div>
-
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="form-check mb-0">
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      value=""
-                      id="form2Example3"
-                    />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="#!" className="text-body">
-                    Forgot password?
-                  </a>
-                </div>
-
-                <div className="text-center text-lg-start mt-4 pt-2">
-                  <button
-                    type="button"
-                    className="btn btn-primary btn-lg"
-                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                  >
-                    Login
-                  </button>
-                  <p className="small fw-bold mt-2 pt-1 mb-0">
-                    Don't have an account?{" "}
-                    <a href="#!" className="link-danger">
-                      Register
-                    </a>
-                  </p>
-                </div>
-              </form>
-              <div className="guest">
-                <Link to="/home">
-                  <button type="button" className="continue-button">
-                    Continue as Guest
-                  </button>
-                </Link>
+                  <small>Sign In with Google</small>
+                </button>
+              </div>
+              <div className="row">
+                <small>
+                  Don't have an account? <a href="#">Sign Up</a>
+                </small>
               </div>
             </div>
           </div>
         </div>
-        <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-          <div>
-            <a href="#!" className="text-white me-4">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a href="#!" className="text-white me-4">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#!" className="text-white me-4">
-              <i className="fab fa-google"></i>
-            </a>
-            <a href="#!" className="text-white">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-        </div>
-      </section>
+      </div>
     </>
   );
 }
